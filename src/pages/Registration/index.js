33adfd87713e841798ab42b991240a84b7dc654a
Gabriel <gabrielratz84@gmail.com>
2021-06-07 11:59:12 +0200

    Router

---
+++ src/pages/Registration/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './styles.scss';
+
+class Registration extends Component {
+  render() {
+    return <div>Reg Page</div>;
+  }
+}
+
+export default Registration;
