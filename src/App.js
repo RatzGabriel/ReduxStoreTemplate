529cf646b204d45028670d7e3d2e62a4a1cdd59e
Gabriel <gabrielratz84@gmail.com>
2021-06-07 14:14:32 +0200

    Layouts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Header from './components/Header';
 import './default.scss';
 import Homepage from './pages/Homepage';
 import Registration from './pages/Registration';
 
+//layouts
+import MainLayout from './Layouts/MainLayout';
+import HomepageLayout from './Layouts/HomepageLayout';
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route path="/registration" component={Registration} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          )}
+        />
+        <Route
+          path="/registration"
+          render={() => (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,25 @@
 import './styles.scss';
 import React from 'react';
 import testLogo from '../../assets/testLogo.jpeg';
+import { Link } from 'react-router-dom';
 
 const Header = (props) => {
   return (
     <header className="header">
       <div className="wrap">
         <div className="logo">
-          <img src={testLogo} alt="Machua Peru Logo" />
+          <Link to="/">
+            <img src={testLogo} alt="Machua Peru Logo" />
+          </Link>
+        </div>
+        <div className="callToActions">
+          <ul>
+            <li>
+              <Link to="/registration" com>
+                Register
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
